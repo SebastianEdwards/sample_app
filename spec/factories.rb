ce60e9b8b9063edf69d58411c9c5943699e36c49
Sebastian Edwards <s.thursday@gmail.com>
2011-03-03 18:16:49 +1300

    Added a User factory

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                     "Sebastian Edwards"
+  user.email                    "s.thursday@gmail.com"
+  user.password                 "password"
+  user.password_confirmation    "password"
+end
