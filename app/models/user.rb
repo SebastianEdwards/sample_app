c32a3ba2968bb6eb825106853aff663e60a3a8d8
Sebastian Edwards <s.thursday@gmail.com>
2011-03-13 13:16:08 +1300

    Can now destory users (as admin)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110228044602
+# Schema version: 20110312232944
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
